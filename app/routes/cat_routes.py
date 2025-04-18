2ec39313dcc523816c9dcd4f3954403dd3ba6942
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2025-04-18 14:02:49 -0400

    Building an API, Part 2: get_one_cat and validate_cat

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import abort, Blueprint, make_response
 from ..models.cat import cats
 
 cats_bp = Blueprint("cats_bp", __name__, url_prefix = "/cats")
@@ -16,4 +16,31 @@ def get_all_cats():
         ))
 
     return results_list
+
+@cats_bp.get("/<id>")
+def get_one_cat(id):
+    cat = validate_cat(id)
+    cat_dict = dict(
+        id = cat.id,
+        name = cat.name,
+        color= cat.color,
+        personality = cat.personality
+    )
+    
+    return cat_dict
+
+def validate_cat(id):
+    try:
+        id = int(id)
+    except ValueError:
+        invalid = {"message": f"Cat id ({id}) is invalid."}
+        abort(make_response(invalid, 400))
+
+    for cat in cats:
+        if cat.id == id:
+            return cat 
+    
+    not_found = {"message": f"Cat with id ({id}) not found."}
+    abort(make_response(not_found, 404))
+
 
