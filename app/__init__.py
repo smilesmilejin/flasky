208d8feaeec4bfbc9a4bc03f525bace493fccbbd
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-18 15:12:20 -0400

    changes based on livecode 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
-# from app.routes.hellow_world_routes import hello_world_bp
-# from .routes.hello_world import hello_world_bp
 from app.routes.cat_routes import cats_bp
+
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
-
-
-    # app.register_blueprint(hello_world_bp)
 
     app.register_blueprint(cats_bp)
 
--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint
-
+from flask import Blueprint, abort, make_response
 # from app.models.cat import cats
 from ..models.cat import cats
 
@@ -7,7 +6,19 @@ import json
 
 cats_bp = Blueprint("cat_bp", __name__, url_prefix = "/cats")
 
-@cats_bp.get("/")
+# Decorator question
+# @cats_bp.get("")
+# It is the same as : @cats_bp.get("", strict_slashes=False)
+
+# Single forward / is the default
+# @cats_bp.get(“”) and @cats_bp.get(“/“): the same route
+    # Always use "/" for the root path of a router.?
+
+# Some frameworks normalize trailing slashes. 
+# FastAPI and Flask can treat /cats and /cats/ the same depending on how you configure them.
+# If you're strict about trailing slashes (strict_slashes=True in Flask), then /cats and /cats/ could be treated differently.
+
+@cats_bp.get("")                                 
 def get_all_cats():
 
     results_list = []
@@ -37,6 +48,37 @@ def get_all_cats():
     
     # return json.dumps(cats_list, indent=2, sort_keys=False)
 
+# <id> is the route parameter
+# like a placeholder for cat id
+        # /cats/1
+# /<id> must match the parameter of get_one_cat(id)
 @cats_bp.get("/<id>")
-def get_one_cat():
-    pass
+def get_one_cat(id):
+    # pass
+    #print(type(id)) # <class 'str'>
+    cat = validate_cat(id)
+
+
+    cat_dict ={
+        "id": cat.id,
+        "name":cat.name,
+        "color": cat.color,
+        "personality": cat.personality
+    }
+    return cat_dict
+
+
+def validate_cat(id):
+    try:
+        id = int(id)
+    except ValueError:
+        invalid = {'message': f"Cat id ({id}) is invalid."}
+        # 400 bad request
+        abort(make_response(invalid, 400))
+
+    for cat in cats:
+        if cat.id == id:
+            return cat
+    
+    not_found = {'message': f"Cat with id ({id}) not found"}
+    abort(make_response(not_found, 404))
