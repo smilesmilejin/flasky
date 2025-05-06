992d7efded9715f50ffff531f73f1d398853e893
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-06 15:19:45 -0400

    changes based on 08 building an API one-to-many part 2 refactoring

--- app/routes/caretaker_routes.py
+++ app/routes/caretaker_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, abort, make_response, request, Response
 from ..db import db
 from app.models.cat import Cat
 from app.models.caretaker import Caretaker
-from .route_utilities import validate_model, create_model
+from .route_utilities import validate_model, create_model, get_models_with_filters
 
 # blueprint is usually bp, when import give it an alis
 bp = Blueprint("caretakers_bp", __name__, url_prefix = "/caretakers")
@@ -12,16 +12,19 @@ bp = Blueprint("caretakers_bp", __name__, url_prefix = "/caretakers")
 @bp.post("")
 def create_caretaker():
     request_body = request.get_json()
-    try:
-        new_caretaker = Caretaker.from_dict(request_body)
-    except KeyError as e:
-        response = {"message": f"Invalid request: missing {e.args[0]}"}
-        abort(make_response(response, 400))
+    return create_model(Caretaker, request_body)
 
-    db.session.add(new_caretaker)
-    db.session.commit()
+    # try:
+    #     new_caretaker = Caretaker.from_dict(request_body)
+    # except KeyError as e:
+    #     response = {"message": f"Invalid request: missing {e.args[0]}"}
+    #     abort(make_response(response, 400))
 
-    return new_caretaker.to_dict(), 201
+    # db.session.add(new_caretaker)
+    # db.session.commit()
+
+    # return new_caretaker.to_dict(), 201
+
 
 # id is the caretaker id
 @bp.post("/<id>/cats")
@@ -31,37 +34,39 @@ def create_cat_with_caretaker_id(id):
 
     request_body["caretaker_id"] = caretaker.id
 
-    # return create_model(Caretaker, request_body)
+    return create_model(Cat, request_body)
 
-    try:
-        new_cat = Cat.from_dict(request_body)
-    except KeyError as e:
-        response = {"message": f"Invalid request: missing {e.args[0]}"}
-        abort(make_response(response, 400))
+    # try:
+    #     new_cat = Cat.from_dict(request_body)
+    # except KeyError as e:
+    #     response = {"message": f"Invalid request: missing {e.args[0]}"}
+    #     abort(make_response(response, 400))
 
-    db.session.add(new_cat)
-    db.session.commit()
+    # db.session.add(new_cat)
+    # db.session.commit()
 
-    return new_cat.to_dict(), 201
+    # return new_cat.to_dict(), 201
+
 
 
 @bp.get("")
 def get_all_caretakers():
-    query = db.select(Caretaker)
+    return get_models_with_filters(Caretaker, request.args)
 
-    name_param = request.args.get("name")
-    if name_param:
-        query = query.where(Caretaker.name == name_param)
+    # query = db.select(Caretaker)
 
-    caretakers = db.session.scalars(query)
+    # name_param = request.args.get("name")
+    # if name_param:
+    #     query = query.where(Caretaker.name == name_param)
 
-    caretakers_response = []
-    for caretaker in caretakers:
-        caretakers_response.append(caretaker.to_dict())
+    # caretakers = db.session.scalars(query)
 
-    return caretakers_response
+    # caretakers_response = []
+    # for caretaker in caretakers:
+    #     caretakers_response.append(caretaker.to_dict())
 
-    # return get_modesl_with_filters(Caretaker, request.args[])
+    # return caretakers_response
+
 
 @bp.get("/<id>/cats")
 def get_all_caretaker_cats(id):
